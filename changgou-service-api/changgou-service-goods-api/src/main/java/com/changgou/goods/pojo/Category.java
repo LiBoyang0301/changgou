70e80198efd2513a4f2114b947f26540b720b682
lby <LiBoyang@qq.com>
2020-12-08 15:05:38 +0800

    2.0

--- changgou-service-api/changgou-service-goods-api/src/main/java/com/changgou/goods/pojo/Category.java
+++ changgou-service-api/changgou-service-goods-api/src/main/java/com/changgou/goods/pojo/Category.java
@@ -2,6 +2,9 @@ package com.changgou.goods.pojo;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -11,6 +14,9 @@ import java.io.Serializable;
  * @Description:Category构建
  * @Date 2019/6/14 19:13
  *****/
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @ApiModel(description = "Category",value = "Category")
 @Table(name="tb_category")
 public class Category implements Serializable{
@@ -41,80 +47,5 @@ public class Category implements Serializable{
 	@ApiModelProperty(value = "模板ID",required = false)
     @Column(name = "template_id")
 	private Integer templateId;//模板ID
-
-
-	//get方法
-	public Integer getId() {
-		return id;
-	}
-
-	//set方法
-	public void setId(Integer id) {
-		this.id = id;
-	}
-	//get方法
-	public String getName() {
-		return name;
-	}
-
-	//set方法
-	public void setName(String name) {
-		this.name = name;
-	}
-	//get方法
-	public Integer getGoodsNum() {
-		return goodsNum;
-	}
-
-	//set方法
-	public void setGoodsNum(Integer goodsNum) {
-		this.goodsNum = goodsNum;
-	}
-	//get方法
-	public String getIsShow() {
-		return isShow;
-	}
-
-	//set方法
-	public void setIsShow(String isShow) {
-		this.isShow = isShow;
-	}
-	//get方法
-	public String getIsMenu() {
-		return isMenu;
-	}
-
-	//set方法
-	public void setIsMenu(String isMenu) {
-		this.isMenu = isMenu;
-	}
-	//get方法
-	public Integer getSeq() {
-		return seq;
-	}
-
-	//set方法
-	public void setSeq(Integer seq) {
-		this.seq = seq;
-	}
-	//get方法
-	public Integer getParentId() {
-		return parentId;
-	}
-
-	//set方法
-	public void setParentId(Integer parentId) {
-		this.parentId = parentId;
-	}
-	//get方法
-	public Integer getTemplateId() {
-		return templateId;
-	}
-
-	//set方法
-	public void setTemplateId(Integer templateId) {
-		this.templateId = templateId;
-	}
-
 
 }
